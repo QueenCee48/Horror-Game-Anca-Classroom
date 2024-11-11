44d3ceb752b1919e83ddde7e5140f6c3727f9203
Ciera <177058032+QueenCee48@users.noreply.github.com>
2024-11-11 13:26:21 -0500

    Update and rename gameStart.js to GameStart.js

--- p5/GameStart.js
+++ p5/GameStart.js
@@ -1,13 +1,13 @@
-function gameStart() {
+function GameStart() {
   
-  this.startButton = new button(0, height/2-40, 80, 80, "");
+  this.startButton = new Button(0, height/2-40, 80, 80, "");
   
   this.draw = function() {
     //start box
     textAlign(LEFT, LEFT);
     fill(0,100,0);
-    textSize(30);
-    text("START",0,height/2-45);
+    textSize(25);
+    text("START",0,height/2-55);
     this.startButton.draw();
   }
   
